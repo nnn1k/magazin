9b8d81c139146e50f6eae67a577a399090fda078
nnn1k <nikitos.elidu@gmail.com>
2025-04-14 01:13:22 +0300

    продукты

--- backend/views/base.py
+++ backend/views/base.py
@@ -4,6 +4,7 @@ from backend.views.test import router as test_router
 from backend.views.auth import router as auth_router
 from backend.views.users import router as users_router
 from backend.views.products import router as product_router
+from backend.views.carts import router as cart_router
 backend_router = APIRouter(
     prefix='/api'
 )
@@ -12,3 +13,4 @@ backend_router.include_router(test_router)
 backend_router.include_router(auth_router)
 backend_router.include_router(users_router)
 backend_router.include_router(product_router)
+backend_router.include_router(cart_router)
